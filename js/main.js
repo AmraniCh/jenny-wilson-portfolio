911f10e2902350dfbcaea869508f5b6a22a87d6b
elamrani744 <elamrani.sv.laza@gmail.com>
2020-06-19 19:47:52 +0100

    Init

---
+++ js/main.js
@@ -0,0 +1,34 @@
+(function() {
+
+    navigationBtnMobileClick();
+    navigationItemsBindClick();
+
+    function navigationBtnMobileClick() {
+        document.querySelector("#menuBtn").addEventListener("click", function() {
+            const nav = document.querySelector("nav.navigation");
+
+            if (this.dataset.toggle === "true") {
+                nav.style.width = "0%";
+                this.dataset.toggle = "false";
+            } else {
+                nav.style.width = "35%";
+                this.dataset.toggle = "true";
+            }
+        });
+    }
+
+    function navigationItemsBindClick() {
+        const links = document.querySelectorAll(".navigation ul li");
+        
+        links.forEach(function(item) {
+            item.addEventListener("click", function() {
+                links.forEach(function(item) {
+                    item.classList.remove('active');
+                });
+                item.classList.toggle('active');
+            });
+        });
+    }
+
+}());
+
